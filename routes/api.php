10c54d3b5ba9735b8752062cb7014befd3e9b222
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-04-07 22:55:00 +0200

    adding routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('register', [AuthController::class, 'register']);
-// Route::post('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+Route::post('logout', [AuthController::class, 'logout']);
+
+});
