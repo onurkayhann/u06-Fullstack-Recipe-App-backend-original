285013f63651b5021bad27e22fcdc0c68736d168
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-04-08 00:11:41 +0200

    adding functionalities

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,20 +8,17 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
-    {
-        $fields = $request->valdiate([
+    public function register(Request $request){
+        $fields = $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|unique:users,email',
             'password' => 'required|string|confirmed'
         ]);
-
         $user = User::create([
             'name' => $fields['name'],
             'email' => $fields['email'],
             'password' => bcrypt($fields['password'])
         ]);
-
         $token = $user->createToken('showapptoken')->plainTextToken;
 
         $response = [
@@ -32,27 +29,24 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
-    public function logout(Request $request)
-    {
-        auth()->user()->tokens()->delete()->;
-
+    public function logout(Request $request) {
+        auth()->user()->tokens()->delete();
         return [
-            'message' => 'logged out'
+            "message" => "logged out"
         ];
     }
 
-    public function login(Request $request)
-    {
+    public function login(Request $request){
         $fields = $request->validate([
             'email' => 'required|string',
-            'password' => 'required|string',
+            'password' => 'required|string'
         ]);
 
         $user = User::where('email', $fields['email'])->first();
 
-        if(!$user || !Hash::check($fields['password'], $user->password)) {
-            return response ([
-                'message' => 'Credentials not correct'
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                "message" => "Credentials not correct"
             ], 401);
         }
 
@@ -65,4 +59,5 @@ class AuthController extends Controller
 
         return response($response, 201);
     }
+
 }
