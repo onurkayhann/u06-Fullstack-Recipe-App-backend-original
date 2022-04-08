439c385647b2e32618c7c975b59047318f8dba35
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-04-08 19:34:59 +0200

    this add will create two models and two migrations

---
+++ app/Models/Recipes.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipes extends Model
+{
+    use HasFactory;
+
+    public function userList()
+    {
+        $this->belongsTo(RecipeList::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function recipeList() {
+        return $this->hasMany(RecipeList::class);
+    }
 }
